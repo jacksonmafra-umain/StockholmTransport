361fe7c47f89f1f98497eed248c7a03d7846721b
Jackson Mafra <jackson.mafra@umain.com>
2025-09-05 21:59:23 +0200

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "StockholmTransport"
+
+pluginManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+include(":shared")
+include(":sample:composeApp")
+
